a3e1ee6c2138c6262bc36874de4e8e271256fd89
M4TE5 <bymatins54@gmail.com>
2022-12-21 15:48:53 +0300

    Creating project

---
+++ app/src/main/java/com/example/birday/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.birday.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.birday.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
