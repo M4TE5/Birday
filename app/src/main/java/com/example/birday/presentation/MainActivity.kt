c14dea92e566797748d548ed733e6ed502958473
M4TE5 <bymatins54@gmail.com>
2022-12-29 16:34:51 +0300

    ViewModel, LiveData

--- app/src/main/java/com/example/birday/presentation/MainActivity.kt
+++ app/src/main/java/com/example/birday/presentation/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.birday.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.birday.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.eventList.observe(this){
+            Log.d("MAVM",it.toString())
+        }
     }
 }
