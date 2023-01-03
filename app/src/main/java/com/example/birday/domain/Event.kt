e8776707adf10d7d982afca37da9bba382f5160e
M4TE5 <bymatins54@gmail.com>
2023-01-03 18:21:57 +0300

    RecyclerView

--- app/src/main/java/com/example/birday/domain/Event.kt
+++ app/src/main/java/com/example/birday/domain/Event.kt
@@ -3,6 +3,7 @@ package com.example.birday.domain
 data class Event(
     val name: String,
     val date: String, //Пока так
+    val showDateTag: Boolean = false,
     var id :Int = UNDEFINED_ID
 ){
     companion object{
