511c85a2aea298193c65ca687fe41be523e219b0
M4TE5 <bymatins54@gmail.com>
2023-02-22 21:06:48 +0300

    Main room database classes

---
+++ app/src/main/java/com/example/birday/data/Event.kt
@@ -0,0 +1,20 @@
+package com.example.birday.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "events")
+data class Event (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ColumnInfo(name = "firstName")
+    var firstName: String,
+    @ColumnInfo(name = "lastName")
+    var lastName: String,
+    @ColumnInfo(name = "date")
+    var date:LocalDate,
+    @ColumnInfo(name = "isFavorite")
+    var isFavorite: Boolean
+        )
